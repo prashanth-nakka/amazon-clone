173b942d5bd5635a41ee8600bfd020742a4cf2d8
Tooni <prashanth04@gmail.com>
2023-04-04 20:26:15 +0100

    Header v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     // BEM Convention
     <div className="app">
-    <h1>Hello, World!</h1>
+    {/* Header */}
+    <Header />
+    {/* Home */}
     </div>
   );
 } 
